a7f90cf7c68cf5339233c7028789245f26231670
Juandreas Ezarfelix <93879577+4EZR@users.noreply.github.com>
2022-03-18 12:28:35 +0700

    Add files via upload

---
+++ Homepage/JS/scripts.js
@@ -0,0 +1,26 @@
+
+var slider_content = document.getElementById('Sl');
+var image = ['SLIDE1','SLIDE2', 'SLIDE3'];
+var i = image.length;
+
+function nextImage(){
+  if (i<image.length) {
+      i= i+1;
+  }else{
+      i = 1;
+  }
+    slider_content.innerHTML = "<a href='Home.html'><img src= Images/"+image[i-1]+".png></a> ";
+}
+
+function prewImage(){
+
+  if (i<image.length+1 && i>1) {
+      i= i-1;
+  }
+  else{
+      i = image.length;
+  }
+slider_content.innerHTML = "<a href='Home.html'><img src= Images/"+image[i-1]+".png></a> ";
+  }
+  setInterval(nextImage , 5000)
+
---
+++ Homepage/JS/zindex.js
@@ -0,0 +1,9 @@
+$(window).on('scroll',function() {;
+
+    if ($(this).scrollTop() >= 100) {
+
+
+        $('.wow').css('z-index', 0);
+       
+    }
+});
