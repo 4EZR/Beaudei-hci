bc526d7fc58c27af9520292f2ce8c9642d8d709a
Juandreas Ezarfelix <93879577+4EZR@users.noreply.github.com>
2022-03-18 15:28:09 +0700

    Update scripts.js

--- Promotion/js/scripts.js
+++ Promotion/js/scripts.js
@@ -8,7 +8,7 @@ images[0]= "images/SLIDE1.PNG";
 images[1]= "images/SLIDE2.PNG";
 images[2]= "images/SLIDE3.png";
 images[3]= "images/SLIDE4.png";
-images[4]="imageS/1.png";
+images[4]="images/1.png";
 
 //Change Image
 function changeImg(){
